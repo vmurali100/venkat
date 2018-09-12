9e296c271c8d560c3a8dbb125bb89c22fbf263e9
venkatareddy127 <gvreddy712@gmail.com>
2018-09-12 18:25:47 +0530

    Crud methods JS

---
+++ data.js
@@ -0,0 +1,92 @@
+var data = [
+    {
+        "fname": "Sandro",
+        "lname": "Curl",
+        "tel": "(853)323-8356",
+        "address": "8667 At Ln",
+        "city": "Atwater",
+        "state": "TX",
+        "zip": 10503
+    },
+    {
+        "fname": "Sharad",
+        "lname": "Isham",
+        "tel": "(133)734-7529",
+        "address": "1504 Sollicitudin Ave",
+        "city": "Hattiesburg",
+        "state": "MI",
+        "zip": 67186
+    },
+    {
+        "fname": "Theresa",
+        "lname": "Delatorre",
+        "tel": "(690)336-4053",
+        "address": "7855 Odio Ln",
+        "city": "Lockport",
+        "state": "MS",
+        "zip": 95408
+    },
+    {
+        "fname": "Ronit",
+        "lname": "Albright",
+        "tel": "(391)168-4750",
+        "address": "6659 Et Ave",
+        "city": "Underwood",
+        "state": "NV",
+        "zip": 50209
+    },
+    {
+        "fname": "Rachelle",
+        "lname": "Jarratt",
+        "tel": "(506)333-2351",
+        "address": "629 Magna St",
+        "city": "Seekonk",
+        "state": "MD",
+        "zip": 43545
+    },
+    {
+        "fname": "Rich",
+        "lname": "Eschenbacher",
+        "tel": "(177)273-7417",
+        "address": "7993 Sed Dr",
+        "city": "Eagle Pass",
+        "state": "WI",
+        "zip": 35903
+    },
+    {
+        "fname": "Geraldine",
+        "lname": "Pfaff",
+        "tel": "(597)513-6497",
+        "address": "8892 Non Ave",
+        "city": "Alhambra",
+        "state": "KS",
+        "zip": 42857
+    },
+    {
+        "fname": "Chris",
+        "lname": "Tollund",
+        "tel": "(306)571-3302",
+        "address": "3438 Dolor St",
+        "city": "Phoenix",
+        "state": "MA",
+        "zip": 29003
+    },
+    {
+        "fname": "Cristina",
+        "lname": "Amacker",
+        "tel": "(219)610-8255",
+        "address": "1221 Sed Rd",
+        "city": "Midlothian",
+        "state": "CA",
+        "zip": 17496
+    },
+    {
+        "fname": "Bridgett",
+        "lname": "Acosta",
+        "tel": "(344)806-0338",
+        "address": "7973 Fringilla St",
+        "city": "Malibu",
+        "state": "NV",
+        "zip": 39823
+    }
+]
