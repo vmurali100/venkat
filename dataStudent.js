d77eafd1688ff94efdd9001cd0bb1e082e77d252
venkatareddy127 <gvreddy712@gmail.com>
2018-09-19 11:47:51 +0530

    JS file for Show student datils events

---
+++ dataStudent.js
@@ -0,0 +1,92 @@
+var data = [
+    {
+        "fname": "Alla",
+        "lname": "Giera",
+        "tel": "(409)605-7855",
+        "address": "2647 Aenean Rd",
+        "city": "San Gabriel Valley",
+        "state": "WA",
+        "zip": 76862
+    },
+    {
+        "fname": "Alan",
+        "lname": "Rovito",
+        "tel": "(997)773-0329",
+        "address": "2771 Dolor Ct",
+        "city": "Palm Beach Gardens",
+        "state": "MN",
+        "zip": 40788
+    },
+    {
+        "fname": "Lontay",
+        "lname": "Jensen",
+        "tel": "(736)425-0130",
+        "address": "2625 Vestibulum Rd",
+        "city": "Roxboro",
+        "state": "KY",
+        "zip": 83106
+    },
+    {
+        "fname": "Marilynn",
+        "lname": "Mcgarity",
+        "tel": "(643)277-6417",
+        "address": "9644 Lorem Ln",
+        "city": "Costa Mesa",
+        "state": "UT",
+        "zip": 41670
+    },
+    {
+        "fname": "Rhonda",
+        "lname": "Chacon",
+        "tel": "(868)016-9129",
+        "address": "9837 Quis Ave",
+        "city": "Malibu",
+        "state": "MS",
+        "zip": 35856
+    },
+    {
+        "fname": "Constantine",
+        "lname": "Wathen",
+        "tel": "(915)682-2599",
+        "address": "8004 Vestibulum Ave",
+        "city": "Sylvania",
+        "state": "RI",
+        "zip": 60397
+    },
+    {
+        "fname": "Charlyne",
+        "lname": "Nesbitt",
+        "tel": "(566)467-4805",
+        "address": "4464 Tempor Ct",
+        "city": "Northern Ca",
+        "state": "VA",
+        "zip": 99829
+    },
+    {
+        "fname": "Lorenzo",
+        "lname": "Norris",
+        "tel": "(108)833-9788",
+        "address": "273 Aenean St",
+        "city": "Torrington",
+        "state": "WI",
+        "zip": 17099
+    },
+    {
+        "fname": "Haytham",
+        "lname": "Bamommy",
+        "tel": "(672)240-2756",
+        "address": "9076 Dolor Ln",
+        "city": "Macomb",
+        "state": "TN",
+        "zip": 52383
+    },
+    {
+        "fname": "Deanna",
+        "lname": "Krebs",
+        "tel": "(409)467-2702",
+        "address": "8024 Pretium Ave",
+        "city": "Costa Mesa",
+        "state": "OH",
+        "zip": 81351
+    }
+]
