227843645fc16ec787020568e728a697357e5c5f
venkatareddy127 <gvreddy712@gmail.com>
2018-09-23 18:47:15 +0530

    Changes made on main page to display current time

--- CRUD-App/main.js
+++ CRUD-App/main.js
@@ -5,3 +5,12 @@ function gotoRegister(){
 function gotoLogin(){
     location.href="login.html"
 }
+
+function showTime(){ //function to display the current time
+    var todaysTime = new Date()
+    document.getElementById("date").innerHTML = todaysTime.toLocaleTimeString();
+}
+showTime();
+setInterval(function (){
+    showTime();
+},1000)
