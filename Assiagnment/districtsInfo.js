c6feb08696b95aa48831d7e83d2f2f0b7b41e176
venkatareddy127 <gvreddy712@gmail.com>
2018-09-26 18:00:15 +0530

    District List

---
+++ Assiagnment/districtsInfo.js
@@ -0,0 +1,8 @@
+var AndhraDivisions = ["Prakasam", "Nelore", "Krishna", "Kurnool", "Kadapa", "Chittor", "Srikakulam", "Vizag"];
+
+var Tamildivisions = ["Chennai", "Coimbatoor", "Madhurai"];
+
+
+var KarnatakaDivsions = ["Bangaluru", "Mangulur", "Mandya", "Coorg", "Mysoor"];
+
+var KeralaDivisions = ["Calicutt", "Trivendrum", "Cochin", "Trissur"];
