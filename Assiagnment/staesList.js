e36227bd6280942af8e294444a210c3585192e5e
venkatareddy127 <gvreddy712@gmail.com>
2018-09-26 13:47:28 +0530

    Assiagnments

---
+++ Assiagnment/staesList.js
@@ -0,0 +1,32 @@
+var StatesInfo = [
+
+    {
+Name:'Andhra',
+Capital: 'Amaravathi',
+Language: 'Telugu',
+Population: 25443433,
+Description: 'Andhra pradesh was seperated as new satate with telangana share the Hyd as same Capital'
+    },
+    {
+        Name:'Tamilnadu',
+        Capital: 'Chennai',
+        Language: 'Tamil',
+        Population: 5645556,
+        Description: 'Tamilnadu best place for Tourists for who visits Temples'
+        },
+        {
+            Name:'Karnataka',
+            Capital: 'Bangalore',
+            Language: 'Kannada',
+            Population: 6543454,
+            Description: 'Karnataka is the great place to produce Software products'
+        },
+        {
+            Name:'Kerala',
+            Capital: 'Trivendram',
+            Language: 'Malayalam',
+            Population: 6568767,
+            Description: 'Kerala is the God Owns country'
+         }
+
+]
